2beac2d657b6b819a6c36f3b60b257c7a2e9c375
O.R <orezek@seznam.cz>
2025-06-20 20:29:51 +0200

    done

--- rendu/exam_rank_06/practice.c
+++ rendu/exam_rank_06/practice.c
@@ -7,20 +7,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-enum {MAX_FD = 65535, RBUF_SIZE = 1000};
+// close fd!!!!
+enum{MAX_FD = 65535, RBUF_SIZE = 1000};
+
+int ids[MAX_FD];
+char *pend[MAX_FD];
 
 int max_fd;
 int next_id;
 
-int ids[MAX_FD + 1];
-char* pend[MAX_FD + 1];
-
-fd_set master, rset, wset;
-
-char rbuf[RBUF_SIZE + 1];
+char rbuf[RBUF_SIZE];
 char wbuf[64];
 
-
+fd_set master, rset, wset;
 
 void fatal(void) {
   write(2, "Fatal error\n", 12);
@@ -54,6 +53,7 @@ int extract_message(char **buf, char **msg)
     return (0);
 }
 
+
 char *str_join(char *buf, char *add)
 {
     char	*newbuf;
@@ -74,34 +74,42 @@ char *str_join(char *buf, char *add)
     return (newbuf);
 }
 
-void broadcast(int except_fd, char*msg) {
-  for(int fd = 0; fd <= max_fd; ++fd) {
-    if (FD_ISSET(fd, &wset) && fd != except_fd)
-      send(fd, msg, strlen(msg), 0);
+
+void broadcast(int except_fd, char* msg)
+{
+  for (int fd = 0; fd <= max_fd; ++fd)
+  {
+      if (FD_ISSET(fd, &wset) && except_fd != fd)
+        send(fd, msg, strlen(msg),0);
   }
 }
 
-void add_client(int fd) {
-  max_fd = (fd > max_fd) ? fd : max_fd;
-  FD_SET(fd, &master);
-  pend[fd] = NULL;
-  ids[fd] = next_id++;
-  sprintf(wbuf, "server: client %d just arrived\n", ids[fd]);
-  broadcast(fd, wbuf);
+void add_client(int fd)
+{
+    max_fd = (max_fd < fd) ? fd : max_fd;
+    FD_SET(fd, &master);
+    ids[fd] = next_id++;
+    pend[fd] = NULL;
+    sprintf(wbuf, "server: client %d just arrived\n", ids[fd]);
+    broadcast(fd, wbuf);
 }
 
-void drop_client(int fd) {
-  sprintf(wbuf, "server: client %d just left\n", ids[fd]);
-  broadcast(fd, wbuf);
-  free(pend[fd]);
-  pend[fd] = NULL;
-  FD_CLR(fd, &master);
+void drop_client(int fd)
+{
+    sprintf(wbuf, "server: client %d just left\n", ids[fd]);
+    broadcast(fd, wbuf);
+    free(pend[fd]);
+    pend[fd] = NULL;
+    FD_CLR(fd, &master);
+    close(fd);
 }
 
-void flush_message(int fd) {
+void flush_messages(int fd)
+{
   char* msg;
   int ret;
-  while((ret = extract_message(&pend[fd], &msg))) {
+  while((ret = extract_message(&pend[fd], &msg)))
+  {
     if (ret == -1)
       fatal();
     sprintf(wbuf, "client %d: ", ids[fd]);
@@ -110,62 +118,85 @@ void flush_message(int fd) {
   }
 }
 
-int get_socket(char* port) {
-  FD_ZERO(&master);
-  struct sockaddr_in servaddr;
-  int s = socket(AF_INET, SOCK_STREAM, 0);
-  if (s == -1)
-    fatal();
+int get_socket(char* port)
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+      fatal();
+    struct sockaddr_in servaddr;
+    bzero(&servaddr, sizeof(servaddr));
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+    servaddr.sin_port = htons(atoi(port));
 
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-  servaddr.sin_port = htons(atoi(port));
-  //bind and listen
-  if (bind(s, (const struct sockaddr*) &servaddr, sizeof(servaddr)) || listen(s, 10))
-    fatal();
-  FD_SET(s, &master);
-  max_fd = s;
-  return (s);
+    if(bind(sockfd,  (const struct sockaddr *)&servaddr, sizeof(servaddr)) || listen(sockfd, 128) == -1)
+      fatal();
+
+    max_fd = sockfd;
+    FD_SET(sockfd, &master);
+    return (sockfd);
 }
 
-int main(int ac, char**avc) {
-  if (ac != 2) {
-    write(2, "Wrong number of arguments\n", 26);
-    return (1);
+int main(int ac, char** avc)
+{
+  if (ac != 2)
+  {
+      write(2,"Wrong number of arguments\n", 26);
+      return (1);
   }
 
   FD_ZERO(&master);
-  int srv = get_socket(avc[1]);
+  int socket = get_socket(avc[1]);
 
-  while(1) {
+  while(1)
+  {
     rset = master;
     wset = master;
-
     if (select(max_fd + 1, &rset, &wset, NULL, NULL) == -1)
       fatal();
 
-    for (int fd = 0; fd <= max_fd; ++fd) {
+    for (int fd = 0; fd <= max_fd; ++fd)
+    {
       if (!FD_ISSET(fd, &rset))
-        continue;
-
-      if (fd == srv) {
-        int cfd = accept(srv, NULL, NULL);
-        if (cfd >= 0)
+          continue;
+      if (socket == fd)
+      {
+        int cfd = accept(socket, NULL, NULL);
+        if (cfd > 0)
           add_client(cfd);
       }
-      else {
+      else
+      {
         int n = recv(fd, rbuf, RBUF_SIZE, 0);
         if (n <= 0)
           drop_client(fd);
-        else {
+        else
+        {
           rbuf[n] = '\0';
           pend[fd] = str_join(pend[fd], rbuf);
           if (!pend[fd])
             fatal();
-          flush_message(fd);
+          flush_messages(fd);
         }
       }
     }
   }
   return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
